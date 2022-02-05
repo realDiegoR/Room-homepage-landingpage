e932350bbc489b86ada16e0897d5925fc8da7a8d
realdiegor <diegoreyes150103@gmail.com>
2022-02-04 23:00:00 -0500

    mobile version finished

---
+++ js/index.js
@@ -0,0 +1,52 @@
+const checkbox = document.getElementById("checkbox")
+const checkboxLabel = document.getElementById("checkbox-label")
+const navBarMobile = document.getElementById("navbarmobile")
+const hero = document.getElementById("main-pic")
+const btn = document.getElementById("btn")
+const btn_left = document.getElementById("btn-left")
+const btn_right = document.getElementById("btn-right")
+const heroImages = {
+    desk_hero_1: "url(./room-homepage-master/images/desktop-image-hero-1.jpg)",
+    desk_hero_2: "url(./room-homepage-master/images/desktop-image-hero-2.jpg)",
+    desk_hero_3: "url(./room-homepage-master/images/desktop-image-hero-3.jpg)",
+    mobile_hero_1: "url(./room-homepage-master/images/mobile-image-hero-1.jpg)",
+    mobile_hero_2: "url(./room-homepage-master/images/mobile-image-hero-2.jpg)",
+    mobile_hero_3: "url(./room-homepage-master/images/mobile-image-hero-3.jpg)",
+
+}
+const hero_1 = "url(./../../room-homepage-master/images/desktop-image-hero-1.jpg)"
+let image = 1
+
+function displayNavMobile() {
+    navBarMobile.classList.toggle("hide")
+}
+
+function changeImageSlider() {
+    // if ()
+}
+
+btn.addEventListener("click", ev => {
+    if (window.screen.availWidth < 562) {
+        if (ev.target == btn_left) {
+            if (image == 1) return false
+            if (image == 3) {
+                hero.style.backgroundImage = heroImages.mobile_hero_2
+            } else if (image == 2) {
+                hero.style.backgroundImage = heroImages.mobile_hero_1
+            }
+            image--
+        } else {
+            if (image == 3) return false
+            if (image == 1) {
+                hero.style.backgroundImage = heroImages.mobile_hero_2
+            } else if (image == 2) {
+                hero.style.backgroundImage = heroImages.mobile_hero_3
+            }
+            image++
+        }
+    } else {
+        if (ev.target == btn_right) {
+            
+        }
+    }
+})
