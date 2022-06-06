13e6a0e7510fe46c8ba76b098dd363518592310d
realdiegor <diegoreyes150103@gmail.com>
2022-06-05 22:26:31 -0400

    refactor

--- js/index.js
+++ js/index.js
@@ -6,21 +6,19 @@ const btn = document.getElementById("btn")
 const btn_left = document.getElementById("btn-left")
 const btn_right = document.getElementById("btn-right")
 let image = 1
-const heroImages = {
-    desk_hero_1: "url(./room-homepage-master/images/desktop-image-hero-1.jpg)",
-    desk_hero_2: "url(./room-homepage-master/images/desktop-image-hero-2.jpg)",
-    desk_hero_3: "url(./room-homepage-master/images/desktop-image-hero-3.jpg)",
-    mobile_hero_1: "url(./room-homepage-master/images/mobile-image-hero-1.jpg)",
-    mobile_hero_2: "url(./room-homepage-master/images/mobile-image-hero-2.jpg)",
-    mobile_hero_3: "url(./room-homepage-master/images/mobile-image-hero-3.jpg)",
-
+const mobileHeroImages = {
+    one: "url(./room-homepage-master/images/mobile-image-hero-1.jpg)",
+    two: "url(./room-homepage-master/images/mobile-image-hero-2.jpg)",
+    three: "url(./room-homepage-master/images/mobile-image-hero-3.jpg)",
+}
+const desktopHeroImages = {
+    one: "url(./room-homepage-master/images/desktop-image-hero-1.jpg)",
+    two: "url(./room-homepage-master/images/desktop-image-hero-2.jpg)",
+    three: "url(./room-homepage-master/images/desktop-image-hero-3.jpg)",
 }
 
-if (window.innerWidth < 562) {
-    hero.style.backgroundImage = heroImages.mobile_hero_1
-} else {
-    hero.style.backgroundImage = heroImages.desk_hero_1
-}
+if (window.innerWidth < 562)  hero.style.backgroundImage = mobileHeroImages.one 
+else hero.style.backgroundImage = desktopHeroImages.one
 
 
 function displayNavMobile() {
@@ -29,49 +27,26 @@ function displayNavMobile() {
 
 btn.addEventListener("click", ev => {
     btn.classList.add("nonclick")
-    setTimeout(timeout, 500)
-    if (window.screen.availWidth < 562) {
-        if (ev.target == btn_left) {
-            if (image == 1) return false
-            if (image == 3) {
-                hero.style.backgroundImage = heroImages.mobile_hero_2
-            } else if (image == 2) {
-                hero.style.backgroundImage = heroImages.mobile_hero_1
-            }
-            image--
-        } else {
-            if (image == 3) return false
-            if (image == 1) {
-                hero.style.backgroundImage = heroImages.mobile_hero_2
-                hero.style.backgroundImage = heroImages.mobile_hero_2
-            } else if (image == 2) {
-                hero.style.backgroundImage = heroImages.mobile_hero_3
-                hero.style.backgroundImage = heroImages.mobile_hero_3
-
-            }
-            image++
-        }
-    } else {
-        if (ev.target == btn_left) {
-            if (image == 1) return false
-            if (image == 3) {
-                hero.style.backgroundImage = heroImages.desk_hero_2
-            } else if (image == 2) {
-                hero.style.backgroundImage = heroImages.desk_hero_1
-            }
-            image--
-        } else {
-            if (image == 3) return false
-            if (image == 1) {
-                hero.style.backgroundImage = heroImages.desk_hero_2
-            } else if (image == 2) {
-                hero.style.backgroundImage = heroImages.desk_hero_3
-            }
-            image++
-        }
-    }
+    setTimeout(enableClick, 500)
+    const screenwidth = window.screen.availWidth
+    if ( screenwidth < 562) changeHeroImage(mobileHeroImages, ev.target)
+    else changeHeroImage(desktopHeroImages, ev.target)
 })
 
-function timeout() {
+function enableClick() {
     btn.classList.remove("nonclick")
+}
+
+function changeHeroImage(setOfImages, button) {
+    if (button == btn_left) {
+        if (image == 1) return false
+        if (image == 2) hero.style.backgroundImage = setOfImages.one
+        if (image == 3) hero.style.backgroundImage = setOfImages.two
+        image--
+    } else {
+        if (image == 3) return false
+        if (image == 1) hero.style.backgroundImage = setOfImages.two
+        if (image == 2) hero.style.backgroundImage = setOfImages.three
+        image++
+    }
 }
